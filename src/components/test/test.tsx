80e347456170a6cc10ff995aded5c74453f07c40
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-10-04 16:19:32 -0600

    initial commit

---
+++ src/components/test/test.tsx
@@ -0,0 +1,11 @@
+import { component$ } from '@builder.io/qwik';
+// import { response } from 'express';
+
+export default component$(() => {
+  
+  return (
+    <div>
+     Hi  Test
+    </div>
+  );
+});
